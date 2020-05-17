3eb825728a15e9d1a6ebad732476ffca402e15be
Robert Leduc <rleduc34@gmail.com>
2020-05-17 00:36:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,9 +12,9 @@ end# add your code snippet here!
 
   def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age =< 12
+    if age < 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    elsif
+    elsif age > 12
     puts "You are too old for this."
     end
   end
